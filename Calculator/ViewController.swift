783713c9e51d2bd46e1aea1ac7b382c7f2069116
Vito Valov <vitovalov@gmail.com>
2017-05-28 22:03:06 +0200

    added display debug printing size lec12

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -28,6 +28,23 @@ class ViewController: UIViewController {
     
     var userIsInTheMiddleOfTyping = false
     
+    private func showSizeClasses() {
+        if !userIsInTheMiddleOfTyping {
+            display.textAlignment = .center
+            display.text = "width " + traitCollection.horizontalSizeClass.description + " height  " + traitCollection.verticalSizeClass.description
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showSizeClasses()
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        coordinator.animate(alongsideTransition: { coordinator in self.showSizeClasses()}, completion: nil)
+    }
+    
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
         
@@ -70,4 +87,24 @@ class ViewController: UIViewController {
     }
     
 }
+
+extension UIUserInterfaceSizeClass: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .compact:
+            return "Compact"
+        case .regular: return "Regular"
+        case .unspecified: return "??"
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
 
